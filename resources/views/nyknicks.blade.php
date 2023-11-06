6eadb6a6a57c9f9a3907c2b813ccf2149054bfe9
darrellathaya <darrell.athaya29@gmail.com>
2023-11-06 15:37:23 +0700

    5026221094

---
+++ resources/views/nyknicks.blade.php
@@ -0,0 +1,108 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="{{asset ('css/knicksstyle.css') }}">
+    <link href='https://fonts.googleapis.com/css?family=Bebas Neue' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200&display=swap');
+    </style>
+
+</head>
+
+
+<body>
+    <header>
+        <nav>
+            <div class="navdiv">
+                <li>
+                    <div class="set">
+                        <i class="fa-solid fa-ellipsis"></i>
+                    </div>
+                </li>
+                <li>
+                    <div class="subs">
+                        <i class="fa-solid fa-bell"></i>
+                        Subscribe
+                    </div>
+                </li>
+            </div>
+        </nav>
+    </header>
+
+    <div>
+        <img class="round-img"
+            src="https://ugc.production.linktr.ee/98WlUXSBQqDyVJbmIPQa_mfg0evlVRoOcShHl?io=true&size=avatar-v1">
+    </div><br>
+
+
+    <div class="text1">
+        NEW YORK KNICKS <i class="fa-solid fa-certificate" style="color:rgb(45, 159, 235); size: 100px;"></i>
+
+    </div><br style="line-height: 30px">
+    <nav class="responsive">
+        <a
+            href="https://www.nba.com/knicks/schedule/schedule-release?cmp=van_nyk_schedule_schedule-release_230817&utm_medium=igbio&utm_source=linktree&utm_campaign=2023-24+schedule+">
+            2023-24 SCHEDULE
+        </a><br style="line-height: 20px">
+        <a href="https://www.youtube.com/watch?v=WWcfLMYGVCM">
+            <div>
+                <span>
+                    <img class="img-shape"
+                        src="https://ugc.production.linktr.ee/b79b3905-4d87-4fdb-a36f-12140dc54326_Career-Remix---John-Starks---1x1.png?io=true&size=thumbnail-stack-v1"
+                        alt="Placeholder image">
+                    <span class="txt-shape">
+                        JOHN STARKS CARREER MIXTAPE
+                    </span>
+                </span>
+            </div>
+        </a><br style="line-height: 20px">
+        <a href="https://www.youtube.com/watch?v=05hGEvo-5G0">
+            <div>
+                <span>
+                    <img class="img-shape"
+                        src="https://ugc.production.linktr.ee/yneA3vTp6N9IQ4y0E4pA_1x1%20(1).png?io=true&size=thumbnail-stack-v1"
+                        alt="Placeholder image">
+                    <span class="txt-shape">
+                        PATRICK EWING CARREER MIXTAPE
+                    </span>
+                </span>
+            </div>
+        </a><br style="line-height: 20px">
+        <a
+            href="https://shop.msg.com/pages/knicks?cmp=osc_nyk_ig-tree_knicks_shop-home_210922&utm_medium=igbio&utm_source=linktree&utm_campaign=shop+knicks+gear#">
+            <div class="left-icon">
+                <i class="fa-solid fa-cart-shopping fa-xl" style="color: #274783;"></i>
+            </div>
+            SHOP KNICKS GEAR
+        </a><br style="line-height: 20px">
+        <a href="https://www.nba.com/knicks/app">
+            <div class="left-icon">
+                <i class="fa-solid fa-mobile-button fa-xl" style="color: #274783;"></i>
+            </div>
+            DOWNLOAD THE KNICKS APP
+        </a><br style="line-height: 20px">
+        <a href="https://www.youtube.com/c/nyknicks">
+            <div class="left-icon">
+                <i class="fa-brands fa-youtube fa-xl" style="color: #274783;"></i>
+            </div>
+            SUBSCRIBE TO OUR YOUTUBE CHANNEL
+        </a><br style="line-height: 30px">
+        <div class="logos">
+            <i class="fa-brands fa-facebook fa-2x" style="color: #ffffff;"></i>&emsp;
+            <i class="fa-brands fa-twitter fa-2x" style="color: #ffffff;"></i>&emsp;
+            <i class="fa-brands fa-instagram fa-2x" style="color: #ffffff;"></i>&emsp;
+            <i class="fa-brands fa-tiktok fa-2x" style="color: #ffffff;"></i>&emsp;
+            <i class="fa-brands fa-youtube fa-2x" style="color: #ffffff;"></i>
+        </div><br style="line-height: 90px">
+        <div class="cookie">
+            Cookie Preferences
+        </div>
+    </nav>
+</body>
+
+</html>
