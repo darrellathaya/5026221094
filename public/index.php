7337ff809c0d22af0ac55b3a202d6d70ebcf7954
darrellathaya <darrell.athaya29@gmail.com>
2023-11-27 14:00:50 +0700

    Perubahan index.php

--- public/index.php
+++ public/index.php
@@ -1,55 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
+</head>
+<body>
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+	<h2>www.malasngoding.com</h2>
+	<h3>Data Pegawai</h3>
 
-define('LARAVEL_START', microtime(true));
+	<a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+	<br/>
+	<br/>
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+	<table border="1">
+		<tr>
+			<th>Nama</th>
+			<th>Jabatan</th>
+			<th>Umur</th>
+			<th>Alamat</th>
+			<th>Opsi</th>
+		</tr>
+		@foreach($pegawai as $p)
+		<tr>
+			<td>{{ $p->pegawai_nama }}</td>
+			<td>{{ $p->pegawai_jabatan }}</td>
+			<td>{{ $p->pegawai_umur }}</td>
+			<td>{{ $p->pegawai_alamat }}</td>
+			<td>
+				<a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
+				|
+				<a href="/pegawai/hapus/{{ $p->pegawai_id }}">Hapus</a>
+			</td>
+		</tr>
+		@endforeach
+	</table>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
 
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+</body>
+</html>
